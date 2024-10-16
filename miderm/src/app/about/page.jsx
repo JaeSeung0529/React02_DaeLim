ffa2da71d0b783e52559e8c8cdcdb5b1a1494aca
JaeSeung0529 <vww2021@naver.com>
2024-10-16 14:14:42 +0900

    about,contact,slug

---
+++ miderm/src/app/about/page.jsx
@@ -0,0 +1,10 @@
+export default function About(){
+    return(
+      <>
+    
+      <h1>D:\202230233 react\React02_DaeLim\miderm\src\app\About\page.jsx</h1>
+      </>
+    )
+  }
+  
+  
---
+++ miderm/src/app/contact/page.jsx
@@ -0,0 +1,10 @@
+export default function Contact(){
+    return(
+      <>
+    
+      <h1>D:\202230233 react\React02_DaeLim\miderm\src\app\contact\page.jsx</h1>
+      </>
+    )
+  }
+  
+  
