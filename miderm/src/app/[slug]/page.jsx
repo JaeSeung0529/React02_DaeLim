5215cd1387c5acfd52215ecb91803ca17f10bddc
JaeSeung0529 <vww2021@naver.com>
2024-10-16 14:19:11 +0900

    3번째 커밋 slug page 동적 라우팅

---
+++ miderm/src/app/[slug]/page.jsx
@@ -0,0 +1,15 @@
+export default function Slug(props){
+    return(
+      <>
+    
+      <h1>D:\202230233 react\React02_DaeLim\miderm\src\app\slug\page.jsx</h1>
+      <br>
+      </br>
+        <div>
+        <h1>App Router</h1>
+        <h1>foo: {props.params.slug}</h1>
+        <h1>bar: {props.searchParams.pid}</h1>
+        </div>
+      </>
+    )
+  }
--- miderm/src/app/slug/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function Slug(){
-    return(
-      <>
-    
-      <h1>D:\202230233 react\React02_DaeLim\miderm\src\app\slug\page.jsx</h1>
-      </>
-    )
-  }
