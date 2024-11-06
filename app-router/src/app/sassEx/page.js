845ac223d6841dbf1f0f0bf8d3de936db7a5b448
JaeSeung0529 <vww2021@naver.com>
2024-11-06 16:49:11 +0900

    .

--- app-router/src/app/sassEx/page.js
+++ app-router/src/app/sassEx/page.js
@@ -1,10 +1,12 @@
 import scss from '@/styles/foo.module.scss'
 
+
 export default function SassEx(){
     return (
         <>
             <div>
                 <h1 className={scss.bar}>SassEx Page</h1>
+                <h2 className={scss.foo}>집에가고싶다</h2>
             </div>
 
         </>
